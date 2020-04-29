4efab929f97cdfd1054c2b16e98a461d9ad4e14a
Natalia Gubanova <khomtali@gmail.com>
2020-04-29 23:43:12 +0200

    updated, homework loops

--- homework-online-store/main.js
+++ homework-online-store/main.js
@@ -44,12 +44,18 @@ let roxyBoard = {
 };
 let goods = [];
 goods.push(burtonBoard, jonesBoard, gnuBoard, roxyBoard);
-
+let goodsInCart = [];
 const productsListEl = document.querySelector('.js-product-list');
-const totalCount = document.querySelector('#cart__count');
-const totalPrice = document.querySelector('#cart__total-price');
-let count = 0, amount = 0;
-function fillInInnerHTML(product) {
+// const totalCount = document.querySelector('#cart__count');
+// const totalPrice = document.querySelector('#cart__total-price');
+const cartBtnEl = document.querySelector('.cart__products-button');
+const totalBtnEl = document.querySelector('.cart__total-button');
+const emptyBtnEl = document.querySelector('.cart__empty-button');
+const goodsInCartListEl = document.querySelector('.cart__js-products');
+const totalEl = document.querySelector('.cart__js-total');
+// let count = 0;
+let amount = 0;
+function makeProductCard(product) {
   return `
     <div class="js-product-card">
       <img src="${product.image}" alt="${product.title}">
@@ -66,18 +72,52 @@ function fillInInnerHTML(product) {
     </div>
   `;
 }
+function makeProductCardInCart(product) {
+  const productCardEl = document.createElement('li');
+  productCardEl.innerHTML = `
+    <span>${product.title}</span><br>
+    <span>Price: $ ${product.price}</span>
+  `;
+  return productCardEl;
+}
 function displayProduct(product) {
   const productItemEl = document.createElement('li');
-  productItemEl.innerHTML = fillInInnerHTML(product);
+  productItemEl.innerHTML = makeProductCard(product);
   productsListEl.appendChild(productItemEl);
-  const addToCartEl = productItemEl.querySelector('.js-product-card__cart-button');
-  addToCartEl.addEventListener('click', (event) => {
-    count += 1;
+  const addToCartBtnEl = productItemEl.querySelector('.js-product-card__cart-button');
+  addToCartBtnEl.addEventListener('click', (event) => {
+    goodsInCart.push(product);
+    addToCartBtnEl.disabled = true;
+    cartBtnEl.disabled = false;
+    totalBtnEl.disabled = false;
+    emptyBtnEl.disabled = false;
+    // count += 1;
     amount += product.price;
-    console.log(amount);
-    totalCount.innerHTML = count;
-    totalPrice.innerHTML = amount;
+    // totalCount.innerHTML = count;
+    // totalPrice.innerHTML = amount;
   });
 }
-
 goods.forEach(displayProduct);
+cartBtnEl.addEventListener('click', () => {
+  goodsInCartListEl.innerHTML = '';
+  goodsInCart.forEach(product => {
+    const productCardEl = makeProductCardInCart(product);
+    goodsInCartListEl.appendChild(productCardEl);
+  });
+});
+totalBtnEl.addEventListener('click', () => {
+  totalEl.innerHTML = `<hr>Total price: $ ${amount}`;
+});
+const addToCartBtnArray = productsListEl.querySelectorAll('button');
+emptyBtnEl.addEventListener('click', () => {
+  goodsInCart = [];
+  goodsInCartListEl.innerHTML = '';
+  totalEl.innerHTML = '';
+  addToCartBtnArray.forEach(button => {
+    button.disabled = false;
+  })
+  cartBtnEl.disabled = true;
+  totalBtnEl.disabled = true;
+  emptyBtnEl.disabled = true;
+  amount = 0;
+});
