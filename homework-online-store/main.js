dc868ea2249c1014bfbb39681aa9921ed4d9fbc5
Natalia Gubanova <khomtali@gmail.com>
2020-05-04 17:28:00 +0200

    responsive shop with cart

--- homework-online-store/main.js
+++ homework-online-store/main.js
@@ -1,4 +1,5 @@
 const burtonBoard = {
+  type: 'Snowboard',
   title: 'Burton REWIND',
   brand: 'burton',
   image: './img/burton_rewind.jpg',
@@ -9,7 +10,8 @@ const burtonBoard = {
     flexRating: 'soft'
   }
 };
-const jonesBoard = {
+const jonesAirheartBoard = {
+  type: 'Snowboard',
   title: 'Jones AIRHEART',
   brand: 'jones',
   image: './img/jones_airheart.png',
@@ -21,6 +23,7 @@ const jonesBoard = {
   }
 };
 const gnuBoard = {
+  type: 'Snowboard',
   title: 'GNU KLASSY C2X',
   brand: 'gnu',
   image: './img/gnu.jpg',
@@ -32,6 +35,7 @@ const gnuBoard = {
   }
 };
 const roxyBoard = {
+  type: 'Snowboard',
   title: 'Roxy TORAH C2',
   brand: 'roxy',
   image: './img/roxy.jpg',
@@ -42,25 +46,40 @@ const roxyBoard = {
     flexRating: 'medium'
   }
 };
+const jonesAviatorBoard = {
+  type: 'Snowboard',
+  title: 'Jones AVIATOR',
+  brand: 'jones',
+  image: './img/jones_aviator.png',
+  price: 496,
+  specification: {
+    terrain: ['Carving', 'All-Mountain'],
+    rockerType: 'Power Camber',
+    flexRating: 'stiff'
+  }
+};
 
 const goods = [];
-goods.push(burtonBoard, jonesBoard, gnuBoard, roxyBoard);
+goods.push(burtonBoard, jonesAirheartBoard, gnuBoard, roxyBoard, jonesAviatorBoard);
 let goodsInCart = [];
-const productsListEl = document.querySelector('.js-product-list');
-const cartBtnEl = document.querySelector('.cart__products-button');
-const totalBtnEl = document.querySelector('.cart__total-button');
-const emptyBtnEl = document.querySelector('.cart__empty-button');
-const goodsInCartListEl = document.querySelector('.cart__js-products');
-const totalEl = document.querySelector('.cart__js-total');
+const goodsListEl = document.querySelector('.js-product-list');
+const cartSnippetEl = document.querySelector('.cart-snippet');
+const badgeEl = cartSnippetEl.querySelector('.cart-snippet__badge');
+const cartEl = document.querySelector('.cart');
+const cartListEl = cartEl.querySelector('.js-cart__list');
+const closeCartEl = cartEl.querySelector('.cart__close');
+const cartAmountEl = cartEl.querySelector('.cart__price');
 let amount = 0;
 
 function makeProductCard(product) {
   return `
     <div class="js-product-card">
-      <img src="${product.image}" alt="${product.title}">
-      <button class="js-product-card__cart-button"></button>
+      <figure>
+        <img src="${product.image}" alt="${product.title}">
+        <button class="js-product-card__cart-button plus"></button>
+      </figure>
       <span class="js-product-card__brand">${product.brand}</span>
-      <h3 id="item-one">Snowboard<br>${product.title}</h3>
+      <h3 id="item-one">${product.type}<br>${product.title}</h3>
       <p>$ ${product.price}</p>
       <div class="js-product-card__specification">
         <h4>Specs</h4>
@@ -72,54 +91,62 @@ function makeProductCard(product) {
   `;
 }
 
-function makeProductCardInCart(product) {
-  const productCardEl = document.createElement('li');
-  productCardEl.innerHTML = `
-    <span>${product.title}</span><br>
-    <span>Price: $ ${product.price}</span>
-  `;
-  return productCardEl;
-}
-
 function displayProduct(product) {
   const productItemEl = document.createElement('li');
   productItemEl.innerHTML = makeProductCard(product);
-  productsListEl.appendChild(productItemEl);
+  goodsListEl.appendChild(productItemEl);
   const addToCartBtnEl = productItemEl.querySelector('.js-product-card__cart-button');
-  addToCartBtnEl.addEventListener('click', (event) => {
-    goodsInCart.push(product);
-    addToCartBtnEl.disabled = true;
-    cartBtnEl.disabled = false;
-    totalBtnEl.disabled = false;
-    emptyBtnEl.disabled = false;
-    amount += product.price;
+  addToCartBtnEl.addEventListener('click', () => {
+    if (addToCartBtnEl.classList.contains('plus')) {
+      goodsInCart.push(product);
+      amount += product.price;
+    } else if (addToCartBtnEl.classList.contains('minus')) {
+      goodsInCart.splice(goodsInCart.indexOf(product), 1);
+      amount -= product.price;
+    }
+    badgeEl.textContent = goodsInCart.length;
+    addToCartBtnEl.classList.toggle('plus');
+    addToCartBtnEl.classList.toggle('minus');
   });
 }
 
 goods.forEach(displayProduct);
-const addToCartBtnArray = productsListEl.querySelectorAll('button');
 
-cartBtnEl.addEventListener('click', () => {
-  goodsInCartListEl.innerHTML = '';
-  goodsInCart.forEach(product => {
-    const productCardEl = makeProductCardInCart(product);
-    goodsInCartListEl.appendChild(productCardEl);
+function makeProductCardInCart(product) {
+  const productCardEl = document.createElement('div');
+  productCardEl.classList.add('js-cart__list__item');
+  productCardEl.innerHTML = `
+    <hr>
+    <img src="${product.image}" alt="${product.title}">
+    <div class="js-cart__list__info">
+      <h4>${product.brand} ${product.type}</h4>
+      <span>${product.title}</span><br>
+      <span>Price: $ ${product.price}</span>
+    </div>
+  `; // <button class="js-cart__list__item__delete">delete</button>
+  return productCardEl;
+}
+
+function toggleVisible(element) {
+  element.classList.toggle('hidden');
+}
+
+closeCartEl.addEventListener('click', () => toggleVisible(cartEl));
+cartSnippetEl.addEventListener('click', event => {
+  event.preventDefault();
+  if(goodsInCart.length == 0) return;
+  cartListEl.innerHTML = '';
+  goodsInCart.forEach(good => {
+    cartListEl.appendChild(makeProductCardInCart(good));
+    // const deleteBtnEl = cartEl.querySelector('.js-cart__list__item__delete');
+    // deleteBtnEl.addEventListener('click', event => {
+    //   console.log(event.target.parentNode);
+    //   const delEl = event.target.parentNode;
+    //   cartListEl.removeChild(delEl);
+    // });
   });
-});
-
-totalBtnEl.addEventListener('click', () => {
-  totalEl.innerHTML = `<hr>Total price: $ ${amount}`;
-});
-
-emptyBtnEl.addEventListener('click', () => {
-  goodsInCart = [];
-  goodsInCartListEl.innerHTML = '';
-  totalEl.innerHTML = '';
-  addToCartBtnArray.forEach(button => {
-    button.disabled = false;
-  });
-  cartBtnEl.disabled = true;
-  totalBtnEl.disabled = true;
-  emptyBtnEl.disabled = true;
-  amount = 0;
+  cartAmountEl.textContent = goodsInCart.reduce((total, product) => {
+    return total + product.price;
+  }, 0);
+  toggleVisible(cartEl);
 });
